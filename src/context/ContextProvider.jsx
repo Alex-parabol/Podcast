806e8d82a71027013f3bd41e9cd852ba5e5c13e3
Cristina Sánchez <cristinasanchez@Cristinas-MacBook-Pro.local>
2023-03-25 17:30:14 +0100

    local storage added in context

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable radix */
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/jsx-no-constructed-context-values */
 import React, { useState, useContext, useEffect } from 'react';
@@ -5,6 +6,26 @@ import PropTypes from 'prop-types';
 import { AppContext } from './AppContext';
 
 export const useAppContext = () => useContext(AppContext);
+
+function parsePodcasts(responseData) {
+  const resultData = responseData.contents;
+  const result = JSON.parse(resultData);
+  return result.feed.entry;
+}
+
+function getPodcastsFromLocalStorage() {
+  const podcasts = localStorage.getItem('podcasts');
+  const lastUpdated = localStorage.getItem('lastUpdated');
+  if (!podcasts || !lastUpdated) return null;
+  const now = new Date().getTime();
+  const timeDiff = now - parseInt(lastUpdated);
+  if (timeDiff > 24 * 60 * 60 * 1000) {
+    localStorage.removeItem('podcasts');
+    localStorage.removeItem('lastUpdated');
+    return null;
+  }
+  return JSON.parse(podcasts);
+}
 
 export function ContextProvider({ children }) {
   const [contextState, setContextState] = useState({
@@ -14,6 +35,10 @@ export function ContextProvider({ children }) {
   });
 
   useEffect(() => {
+    const storedPodcasts = getPodcastsFromLocalStorage();
+    if (storedPodcasts) {
+      setContextState({ ...contextState, podcasts: storedPodcasts, loading: false });
+    }
     fetch(
       `https://api.allorigins.win/get?url=${encodeURIComponent(
         'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json',
@@ -21,8 +46,10 @@ export function ContextProvider({ children }) {
     )
       .then((response) => response.json())
       .then((data) => {
-        const finalData = JSON.parse(data.contents).feed.entry;
-        setContextState({ ...contextState, podcasts: finalData, loading: false });
+        const parsedData = parsePodcasts(data);
+        setContextState({ ...contextState, podcasts: parsedData, loading: false });
+        localStorage.setItem('podcasts', JSON.stringify(parsedData));
+        localStorage.setItem('lastUpdated', new Date().getTime().toString());
       });
   }, []);
 
---
+++ src/context/utils/podcastFunctions.js
@@ -0,0 +1,19 @@
+export function parsePodcasts(responseData) {
+  const resultData = responseData.contents;
+  const result = JSON.parse(resultData);
+  return result.feed.entry;
+}
+
+export function getPodcastsFromLocalStorage() {
+  const podcasts = localStorage.getItem('podcasts');
+  const lastUpdated = localStorage.getItem('lastUpdated');
+  if (!podcasts || !lastUpdated) return null;
+  const now = new Date().getTime();
+  const timeDiff = now - parseInt(lastUpdated);
+  if (timeDiff > 24 * 60 * 60 * 1000) {
+    localStorage.removeItem('podcasts');
+    localStorage.removeItem('lastUpdated');
+    return null;
+  }
+  return JSON.parse(podcasts);
+}
